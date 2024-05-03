275a2d99e29711f8dc2d20dd0c21d3726745685f
Shrawan2002 <shrawankumar1754@gmail.com>
2024-05-03 23:01:02 +0530

    updates files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector("button");
 let inp = document.querySelector("input");
 let ul = document.querySelector("ul");
+console.log(ul);
 
 btn.addEventListener("click", function(){
     console.log("button was click");
@@ -13,4 +14,13 @@ btn.addEventListener("click", function(){
     item.appendChild(delBtn);
     ul.appendChild(item);
     inp.value="";
+})
+
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem= event.target.parentElement;
+        listItem.remove();
+        console.log("button was deleted");
+    }
+
 })
